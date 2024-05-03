8c893bb553ce825c98aef38652a8d5e1da19c79c
Amrith-H-N <amrith.harijayanthan-namboodiri.3771@student.uu.se>
2024-05-03 14:50:46 +0200

    trigger docs build

--- sensor_node/driver/ds18b20.h
+++ sensor_node/driver/ds18b20.h
@@ -12,7 +12,7 @@
 
 #include "sensordata.h"
 
-#define DS18B20_RESOLUTION 10  // 10 bit resolution
+#define DS18B20_RESOLUTION 10 // 10 bit resolution
 
 // DS18B20 Function Commands
 #define DS18B20_FN_CONVERT_T_CMD 0x44
@@ -29,14 +29,16 @@
 #define DS18B20_SKIP_ROM_CMD 0xCC
 #define DS18B20_ALARMSEARCH_ROM_CMD 0xEC
 
-typedef struct {
-  const struct device *addr;  // device tree address
+typedef struct
+{
+  const struct device *addr; // device tree address
   uint8_t family;
   uint8_t resolution;
 } ds18b20_config_t;
 // RAM sscratchpad structure
-typedef struct ds18b20_scratchpad {
-  int16_t temperature;  // in degree celcius
+typedef struct ds18b20_scratchpad
+{
+  int16_t temperature; // in degree celcius
   uint8_t TH_REGISTER;
   uint8_t TL_REGISTER;
   uint8_t config_register;
@@ -44,11 +46,12 @@ typedef struct ds18b20_scratchpad {
   uint8_t crc;
 } ds18b20_scratchpad_t;
 //
-typedef struct ds18b20_data {
-  struct w1_slave_config config;    // rom data
-  ds18b20_scratchpad_t scratchpad;  // our scratchpad
+typedef struct ds18b20_data
+{
+  struct w1_slave_config config;   // rom data
+  ds18b20_scratchpad_t scratchpad; // our scratchpad
 } ds18b20_data_t;
 
-// void ds18b20_set_resolution(dev, ds18b20->resolution);
+// void ds18b20_set_resolution(dev, ds18b20->resolution); ...
 
 #endif
